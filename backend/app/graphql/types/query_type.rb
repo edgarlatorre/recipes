46eb3d9bdd2bea126736eeded4b7748c936e20a1
Edgar Latorre <me@edgarlatorre.com.br>
2023-07-22 21:54:32 +0200

    Configure graphql and recipe type

---
+++ backend/app/graphql/types/query_type.rb
@@ -0,0 +1,17 @@
+module Types
+  class QueryType < Types::BaseObject
+    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
+    include GraphQL::Types::Relay::HasNodeField
+    include GraphQL::Types::Relay::HasNodesField
+
+    # Add root-level fields here.
+    # They will be entry points for queries on your schema.
+
+    # TODO: remove me
+    field :recipes, [Types::RecipeType], null: false,
+      description: "Returns recipes"
+    def recipes
+      Recipe.all
+    end
+  end
+end
---
+++ backend/app/graphql/types/recipe_type.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Types
+  class RecipeType < Types::BaseObject
+    field :id, ID, null: false
+    field :title, String, null: false
+    field :ratings, Float
+    field :cook_time, Float
+    field :preparation_time, Float
+    field :image, String
+    field :author, String
+    field :cuisine, String
+    field :category_id, Integer
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
